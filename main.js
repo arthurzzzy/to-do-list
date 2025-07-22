30837a1dfcae8619587f3b3e3d54eccc0943c99d
Arthur Leoncio <arthurleonciobs@gmail.com>
2025-07-21 23:21:52 -0300

    Corpo do site finalizado

--- main.js
+++ main.js
@@ -4,23 +4,43 @@ function addTask() {
     const taskText = input.value.trim();
 
     if (taskText === "") return;
-    
-    
+
     const li = document.createElement("li");
-    li.textContent = taskText;
+
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+    
+    span.style.cursor = "pointer"; 
+
+
+    span.addEventListener("click", function () {
+        span.classList.toggle("completed");
+    });
+
+ 
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.onclick = function () {
+        li.remove(); 
+    };
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
 
     const ul = document.getElementById("taskList");
     ul.appendChild(li);
-    input.value = ""
+
+    input.value = "";
 }
 
-document.getElementById("taskInput").addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
+
+document.getElementById("taskInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
         addTask();
     }
-})
+});
 
- document.getElementById('todoList').innerHTML = '';
- document.getElementById('todoInput').value = '';
 
- 
+
